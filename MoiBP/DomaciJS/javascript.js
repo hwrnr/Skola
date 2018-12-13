d53271bd820e5267b9d5a819d6440cf9e660db34
hwrnr <hawerner.hawerner@gmail.com>
2018-12-13 19:52:52 +0100

    Domaci sa div

--- MoiBP/DomaciJS/javascript.js
+++ MoiBP/DomaciJS/javascript.js
@@ -7,7 +7,35 @@ var filmovi = [
   },
   {
     "name" : "Zvezda je rodjena", "img" : "film3.jpg", "link" : "film3.html"
-  }
+  },
+
+  {
+    "name" : "Brzi Brzic", "img" : "film1.jpg", "link" : "film1.html"
+  },
+  {
+    "name" : "Park uzasa", "img" : "film2.jpg", "link" : "film2.html"
+  },
+  {
+    "name" : "Zvezda je rodjena", "img" : "film3.jpg", "link" : "film3.html"
+  },
+  {
+    "name" : "Brzi Brzic", "img" : "film1.jpg", "link" : "film1.html"
+  },
+  {
+    "name" : "Park uzasa", "img" : "film2.jpg", "link" : "film2.html"
+  },
+  {
+    "name" : "Zvezda je rodjena", "img" : "film3.jpg", "link" : "film3.html"
+  },
+  {
+    "name" : "Brzi Brzic", "img" : "film1.jpg", "link" : "film1.html"
+  },
+  {
+    "name" : "Park uzasa", "img" : "film2.jpg", "link" : "film2.html"
+  },
+  {
+    "name" : "Zvezda je rodjena", "img" : "film3.jpg", "link" : "film3.html"
+  },
 ]
 function getData() {
   return filmovi;
@@ -15,14 +43,16 @@ function getData() {
 
 function arrayToTable(tableData) {
     console.log(tableData);
-    var table = $('<table></table>');
+    var table = $('<div class="fluid-container"></div>');
+    var red = $('<div class="row"> </div>')
     $(tableData).each(function (i, rowData) {
-        var row = $('<tr></tr>');
+        var row = $('<div class="col-md-3"></div>');
         $(rowData).each(function (j, cellData) {
-            row.append($('<td>' + cellData["name"] + '</td>' + '<td width="200px">' + '<a href="' + cellData["link"] + '"><img src="' + cellData["img"] + '"/></a> </td>'));
+            row.append($('<p>' + cellData["name"] + '</p>' + '<a href="' + cellData["link"] + '"><img src="' + cellData["img"] + '"/></a>'));
         });
-        table.append(row);
+        red.append(row);
     });
+    table.append(red);
     return table;
 }
 
@@ -30,4 +60,4 @@ function onLoadTable(){
   $('body').append(arrayToTable(getData()));
 }
 
- $(document).ready(onLoadTable());
+ // $(document).ready(onLoadTable());
